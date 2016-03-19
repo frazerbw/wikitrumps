bb1a17600e04dd398985e0af0a66624b31bc528f
Darren Vong <d.vongy@gmail.com>
2016-03-19 21:55:23 +0000

    Access-Control-Allow-Origin set to * to see if it allows ajax requests on heroku

--- server/router.py
+++ server/router.py
@@ -1,18 +1,26 @@
 # -*- coding: utf-8 -*-
 from sys import argv
-from bottle import request, template, run, route, post
+from bottle import request, template, run, route, post, static_file, TEMPLATE_PATH,\
+    response
 
 import page_processor
 
+TEMPLATE_PATH.append('../client')
+
 @route('/')
 def index():
-    return template("test")
+    return template("index")
 
 @route('/data')
 def get_data():
+    response.set_header("Access-Control-Allow-Origin", "*")
     page_title = request.query.get("page_title")
     print page_title
     return page_processor.get_page_data(page_title)
+
+@route('/<path:path>')
+def get_resources(path):
+    return static_file(path, root="../client")
 
 if __name__ == '__main__':
     run(host='0.0.0.0', debug=True, port=argv[1], reloader=True)
